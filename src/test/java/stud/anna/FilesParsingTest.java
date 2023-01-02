52343b01f125f03ff9562d2ad5f38561285e1af1
Anna Petrova <mail.anna.p@yandex.ru>
2023-01-02 13:33:12 +0500

    add files

--- src/test/java/stud/anna/FilesParsingTest.java
+++ src/test/java/stud/anna/FilesParsingTest.java
@@ -2,12 +2,16 @@ package stud.anna;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
+import stud.anna.model.Glossary;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -41,6 +45,61 @@ public class FilesParsingTest {
         try (InputStream resourceAsStream = cl.getResourceAsStream("Журнал по листам ожидания-2022-11-02T16-01-48.xlsx")) {
             XLS content = new XLS(resourceAsStream);
             assertThat(content.excel.getSheetAt(0).getRow(1).getCell(0).getStringCellValue()).contains("Журнал по листам ожидания от 02.11.2022");
+        }
+
+    }
+
+    @Test
+    void csvParseTest() throws Exception {
+        try (
+                InputStream resource = cl.getResourceAsStream("qa_guru.csv");
+                CSVReader reader = new CSVReader(new InputStreamReader(resource))
+        ) {
+            List<String[]>content = reader.readAll();
+
+            assertThat(content.get(0)[1]).contains("lesson");
+        }
+    }
+
+    @Test
+    void zipParseTest() throws Exception {
+        try (
+                InputStream resource = cl.getResourceAsStream("sample.zip");
+                ZipInputStream zis = new ZipInputStream(resource)
+        ) {
+            ZipEntry entry;
+            while((entry = zis.getNextEntry()) != null) {
+    assertThat(entry.getName()).isEqualTo("sample.txt");
+            }
+        }
+    }
+
+    @Test
+    void jsonParseTest() throws Exception {
+        Gson gson = new Gson();
+        try (
+                InputStream resource = cl.getResourceAsStream("glossary.json");
+                InputStreamReader reader = new InputStreamReader(resource)
+        ) {
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+            assertThat(jsonObject.get("title").getAsString()).isEqualTo("example glossary");
+            assertThat(jsonObject.get("GlossDiv").getAsJsonObject().get("title").getAsString()).isEqualTo("S");
+            assertThat(jsonObject.get("GlossDiv").getAsJsonObject().get("flag").getAsBoolean()).isTrue();
+        }
+
+    }
+
+    @Test
+    void jsonParseImprovedTest() throws Exception {
+        Gson gson = new Gson();
+        try (
+                InputStream resource = cl.getResourceAsStream("glossary.json");
+                InputStreamReader reader = new InputStreamReader(resource)
+        ) {
+            Glossary jsonObject = gson.fromJson(reader, Glossary.class);
+            assertThat(jsonObject.title).isEqualTo("example glossary");
+            assertThat(jsonObject.glossDiv.title).isEqualTo("S");
+            assertThat(jsonObject.glossDiv.flag).isTrue();
         }
 
     }
---
+++ src/test/java/stud/anna/model/Glossary.java
@@ -0,0 +1,15 @@
+package stud.anna.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Glossary {
+    public String title;
+    @SerializedName("GlossDiv")
+    public GlossDiv glossDiv;
+
+    public static class GlossDiv {
+        public String title;
+        public boolean flag;
+
+    }
+}
